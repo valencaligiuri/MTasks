dca2205a9c4d81b42929bb401276a080a81662c6
Makito <valencaligiuri06@gmail.com>
2025-04-09 23:41:18 -0300

    Remove obsolete Tailwind CSS configuration and components; add HelloWorld component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,33 +1,15 @@
 <template>
-  <div :class="{'dark': isDarkMode}">
-    <router-view @toggleDarkMode="checkTheme"/>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  data() {
-    return {
-      isDarkMode: false,
-    }
-  },
-  mounted() {
-    this.checkTheme()
-  },
-  watch: {
-    $route() {
-      this.checkTheme()
-    }
-  },
-  methods: {
-    checkTheme() {
-      const savedTheme = localStorage.getItem("darkMode");
-      if (savedTheme) {
-        this.isDarkMode = savedTheme === 'true';
-      } else {
-        this.isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-      }
-    }
+  name: 'App',
+  components: {
+    HelloWorld
   }
 }
 </script>
@@ -39,7 +21,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-
+  margin-top: 60px;
 }
-
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,4 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import '@/assets/tailwind.css'
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+createApp(App).mount('#app')
